7a650af07597af79ea61c2f530f1cee580d34e10
igor ujiie <igorkazuhico@hotmail.com>
2022-06-16 20:06:42 -0300

    Finalizando o Projeto!

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,44 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ListaCategorias from '../components/ListaCategorias'
-import '../assets/css/blog.css'
 import ListaPost from '../components/ListaPost'
-import { Link, Route, useParams, useRouteMatch} from 'react-router-dom'
+import '../assets/css/blog.css'
+import { Route, useParams, useRouteMatch, Link, Switch } from 'react-router-dom'
 import { busca } from '../api/api'
+import SubCategoria from '../paginas/SubCategoria'
+
 
 const Categoria = () => {
-    const { id } = useParams()
-    const { url, path } = useRouteMatch()
-    const [subcategoria, setSubCategorias] = useState([])
+  const { id } = useParams()
+  const { url, path } = useRouteMatch()
+  const [subcategorias, setSubCategorias] = useState([])
 
-    useEffect(() => {
-        busca(`/categorias/${id}`, (categoria) => {
-            setSubCategorias(categoria.subcategorias)
-        })
-    }, [id])
-    return (
-        <>
-            <div className="container">
-                <h2 className="titulo-pagina">Pet Notícias</h2>
-            </div>
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubCategorias(categoria.subcategorias)
+    })
+  }, [id])
+  return (
+    <>
+      <div className="container">
+        <h2 className="titulo-pagina">Pet Notícias</h2>
+      </div>
 
-            <ListaCategorias />
-            <ul className="lista-categorias container flex">
-                {
-                    subcategoria.map((subcategoria) => (
-                        <li className={`lista-categorias__categoria 
-                        lista-categorias__categoria--${id}`} key={subcategoria}>
-                            <Link to={`${url}/${subcategoria}`}>
-                                {subcategoria}
-                            </Link>
-                        </li>
-                    ))
-                }
-            </ul>
-            <Route exact path={`${path}/`}>
-                <ListaPost url={`/posts?categoria=${id}`} />
-            </Route>
-        </>
-    )
+      <ListaCategorias />
+      <ul className="lista-categorias container flex">
+        {
+          subcategorias.map((subcategoria) => (
+            <li
+              className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+              key={subcategoria}
+            >
+              <Link to={`${url}/${subcategoria}`}>
+                {subcategoria}
+              </Link>
+            </li>
+          ))
+
+        }
+      </ul>
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <ListaPost url={`/posts?categoria=${id}`} />
+        </Route>
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
+    </>
+  )
 }
-
 export default Categoria
